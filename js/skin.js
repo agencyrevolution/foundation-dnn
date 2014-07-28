2c762e6ed284faa6e66600464ae97d503ba43161
Stephen Way <stephencway@me.com>
2014-07-28 13:55:44 -0700

    Update skin.js

--- js/skin.js
+++ js/skin.js
@@ -1,8 +1,10 @@
+/*global $:false */
 // Load Foundation
 $(document).foundation();
 
 // Document Ready
 $(function() {
+  'use strict';
   // $('.slider').slick({
   //   autoplay: true,
   //   speed: 300
