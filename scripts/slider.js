1e6127166fb1008bdf4da79878a377f53dc115fb
Stephen Way <stephenway@users.noreply.github.com>
2015-02-18 14:05:19 -0800

    Update slider.js

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,15 +1,10 @@
-/*global $:false;*/
-
-document.addEventListener('DOMContentLoaded', function() {
-  'use strict';
-  $('.slider-main').slick({
-    autoplay: true,
-    autoplaySpeed: 5000,
-    dots: true,
-    fade: true,
-    infinite: true,
-    speed: 300,
-    slide: 'div',
-    cssEase: 'linear'
-  });
+$('.slider-main').slick({
+  autoplay: true,
+  autoplaySpeed: 5000,
+  dots: true,
+  fade: true,
+  infinite: true,
+  speed: 300,
+  slide: 'div',
+  cssEase: 'linear'
 });
