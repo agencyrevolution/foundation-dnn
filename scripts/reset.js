e3b299082279579630dce938b635548b7c79bbd4
Stephen Way <stephenway@users.noreply.github.com>
2015-02-18 13:41:12 -0800

    Update reset.js

--- scripts/reset.js
+++ scripts/reset.js
@@ -1,20 +1,18 @@
 /*global $:false */
 
-document.addEventListener('DOMContentLoaded', function() {
-  'use strict';
+'use strict';
 
-  // Reset Quote Form
-  $('input, textarea').placeholder();
-  $(':input','.addressAdv').attr('placeholder', 'Apt.');
-  // convert quote form labels to placeholders
-  $(':input','.field').each(function(index, elem) {
-    var eId = $(elem).attr('id');
-    var label = null;
-    if (eId && (label = $(elem).parents('form').find('label[for="+eId+"]')).length === 1) {
-        $(elem).attr('placeholder', $(label).html());
-        $(label).remove();
-    }
-  });
-  // remove inline styles from state select
-  $('select', '.state').removeAttr( 'style' );
+// Reset Quote Form
+$('input, textarea').placeholder();
+$(':input','.addressAdv').attr('placeholder', 'Apt.');
+// convert quote form labels to placeholders
+$(':input','.field').each(function(index, elem) {
+  var eId = $(elem).attr('id');
+  var label = null;
+  if (eId && (label = $(elem).parents('form').find('label[for="+eId+"]')).length === 1) {
+      $(elem).attr('placeholder', $(label).html());
+      $(label).remove();
+  }
 });
+// remove inline styles from state select
+$('select', '.state').removeAttr( 'style' );
