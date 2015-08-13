b865aa51fb56e1685ecee2e68505306fdffaa17f
Stephen Way <stephenway@users.noreply.github.com>
2015-08-13 11:37:22 -0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -18,49 +18,6 @@ gulp.task('lint', function() {
     .pipe(eslint.failOnError());
 });
 
-// Browserify task
-gulp.task('browserify', function() {
-  // Single point of entry
-  gulp.src([ './js/main.js' ])
-  .pipe(browserify({ insertGlobals: true }))
-  // Bundle to a single file
-  .pipe(concat('skin.js'))
-  // Output it to our dist folder
-  .pipe(gulp.dest('./dist/js/'));
-});
-
-// Browserify task
-gulp.task('browserify-min', function() {
-  // Single point of entry
-  gulp.src([ './js/main.js' ])
-  .pipe(browserify({
-    insertGlobals: true,
-    debug: true
-  }))
-  // Bundle to a single file
-  .pipe(concat('skin.js'))
-  // Output it to our dist folder
-  .pipe(gulp.dest('./dist/js/'))
-  .pipe(uglify())
-  .pipe(rename({ suffix: '.min' }))
-  .pipe(gulp.dest('./dist/js/'));
-});
-
-gulp.task('stylesheets', function() {
-  gulp.src('./styles/*.scss')
-    .pipe(sass({onError: function(e) { console.log(e); },
-      includePaths: [
-        'node_modules/foundation-sites/scss',
-        'node_modules/utility-belt/scss',
-        'node_modules/slick-carousel/slick'
-      ]}))
-    .pipe(prefix('last 2 versions'))
-    .pipe(gulp.dest('./dist/css/'))
-    .pipe(minifyCSS())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('./dist/css/'));
-});
-
 gulp.task('minifyJS', function() {
   gulp.src(['dist/js/*.js', '!dist/js/*.min.js'])
   .pipe(uglify())
@@ -75,32 +32,45 @@ gulp.task('minifyCSS', function() {
     .pipe(gulp.dest('./dist/css/'));
 });
 
-gulp.task('minify', ['minifyJS','minifyCSS']);
+gulp.task('minify', ['minifyJS', 'minifyCSS']);
 
-gulp.task('watch', ['lint'], function() {
-  gulp.watch(['./js/*.js', './js/**/*.js'],[
+// Browserify task
+gulp.task('browserify', function() {
+  // Single point of entry
+  gulp.src([ './js/main.js' ])
+  .pipe(browserify({ insertGlobals: true }))
+  // Bundle to a single file
+  .pipe(concat('skin.js'))
+  // Output it to our dist folder
+  .pipe(gulp.dest('./dist/js/'));
+});
+
+gulp.task('stylesheets', function() {
+  gulp.src('./styles/*.scss')
+    .pipe(sass({onError: function(e) { console.log(e); },
+      includePaths: [
+        'node_modules/foundation-sites/scss',
+        'node_modules/utility-belt/scss',
+        'node_modules/slick-carousel/slick'
+      ]}))
+    .pipe(prefix('last 2 versions'))
+    .pipe(gulp.dest('./dist/css/'));
+});
+
+
+gulp.task('watch', function() {
+  gulp.watch(['./js/**/*.js'], [
     'lint',
-    'browserify'
+    'minifyJS'
   ]);
-  gulp.watch(['./styles/*.scss', './styles/**/*.scss'], [
-    'stylesheets'
+  gulp.watch(['./styles/**/*.scss'], [
+    'stylesheets',
+    'minifyCSS'
   ]);
 });
 
-gulp.task('watch-all', ['lint'], function() {
-  gulp.watch(['./js/*.js', './js/**/*.js'],[
-    'lint',
-    'browserify-min'
-  ]);
-  gulp.watch(['./styles/*.scss', './styles/**/*.scss'], [
-    'stylesheets'
-  ]);
-});
+gulp.task('default', ['browserify', 'stylesheets']);
 
-gulp.task('default', ['browserify','stylesheets']);
+gulp.task('dev', ['default', 'watch']);
 
-gulp.task('dev', ['default','watch']);
-
-gulp.task('build', ['default','minify']);
-
-gulp.task('build-watch', ['default','watch-all']);
+gulp.task('build', ['default', 'minify']);
